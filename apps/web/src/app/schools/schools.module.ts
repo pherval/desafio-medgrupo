dc1f7bc1a759136fb7368a003766c8be3c70a8a7
Peter <github@peter.dev.br>
2022-05-18 00:31:27 -0300

    feat(web): read all schools from backend

--- apps/web/src/app/schools/schools.module.ts
+++ apps/web/src/app/schools/schools.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SchoolsComponent } from './schools.component';
 import { SchoolsRoutingModule } from './schools-routing.module';
+import { SchoolsService } from './schools.service';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SchoolsComponent],
   imports: [CommonModule, SchoolsRoutingModule, SharedModule],
+  providers: [SchoolsService],
 })
 export class SchoolsModule {}
---
+++ apps/web/src/app/schools/schools.service.ts
@@ -0,0 +1,97 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+enum Operation {
+  deactivated = 0,
+  activated,
+}
+
+export type School = {
+  id: number;
+  name: string;
+  cie: string;
+  status: Operation;
+  contacts: {
+    phones: string[];
+    email?: string;
+  };
+
+  supervisor: string;
+
+  address: {
+    street: string;
+    complement?: string;
+    zipCode: string;
+  };
+};
+
+type SchoolDTO = {
+  id: number;
+  nome: string;
+  cie: string;
+  endereco: string;
+  cep: string;
+  situacao_funcionamento: 'ativo' | 'desativado';
+  supervisor: string;
+  telefones: string[];
+  email?: string;
+  complemento?: string;
+};
+
+@Injectable()
+export class SchoolsService {
+  private static BASE_URL = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  all(): Observable<School[]> {
+    return this.http
+      .get<SchoolDTO[]>(SchoolsService.resourceUrl)
+      .pipe(map((schools) => schools.map(this.mapper)));
+  }
+
+  private static get baseUrl(): string {
+    return this.BASE_URL;
+  }
+
+  private static get resourceUrl(): string {
+    return `${this.baseUrl}/escolas`;
+  }
+
+  private mapper({
+    id,
+    nome,
+    cep,
+    cie,
+    supervisor,
+    telefones,
+    situacao_funcionamento,
+    endereco,
+    email,
+    complemento,
+  }: SchoolDTO): School {
+    return {
+      id,
+      name: nome,
+      cie,
+      status:
+        situacao_funcionamento === 'ativo'
+          ? Operation.activated
+          : Operation.deactivated,
+      contacts: {
+        phones: telefones,
+        email,
+      },
+
+      address: {
+        street: endereco,
+        zipCode: cep,
+        complement: complemento,
+      },
+
+      supervisor,
+    };
+  }
+}

8519310346d0a9ac5f5023123be0c22072214335
Peter <github@peter.dev.br>
2022-05-18 12:56:59 -0300

    refactor(web): move card from schools to shared module

--- apps/web/src/app/schools/schools.module.ts
+++ apps/web/src/app/schools/schools.module.ts
@@ -2,21 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SchoolsComponent } from './schools.component';
 import { SchoolsRoutingModule } from './schools-routing.module';
-import { CardComponent } from './card/card.component';
 import { SchoolsService } from './schools.service';
 import { SharedModule } from '../shared/shared.module';
 import { NewSchoolComponent } from './new-school/new-school.component';
 import { ListingSchoolsComponent } from './listing-schools/listing-schools.component';
-import { CardSectionComponent } from './card/card-section/card-section.component';
 
 @NgModule({
-  declarations: [
-    SchoolsComponent,
-    CardComponent,
-    NewSchoolComponent,
-    ListingSchoolsComponent,
-    CardSectionComponent,
-  ],
+  declarations: [SchoolsComponent, NewSchoolComponent, ListingSchoolsComponent],
   imports: [CommonModule, SchoolsRoutingModule, SharedModule],
   providers: [SchoolsService],
 })
--- apps/web/src/app/shared/shared.module.ts
+++ apps/web/src/app/shared/shared.module.ts
@@ -4,11 +4,24 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TypeAheadService } from './type-ahead.service';
 import { ButtonDirective } from './button.directive';
 import { SearchInputComponent } from './search-input/search-input.component';
+import { CardComponent } from './card/card.component';
+import { CardSectionComponent } from './card/card-section/card-section.component';
 
 @NgModule({
-  declarations: [ButtonDirective, SearchInputComponent],
+  declarations: [
+    ButtonDirective,
+    SearchInputComponent,
+    CardComponent,
+    CardSectionComponent,
+  ],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [FontAwesomeModule, ButtonDirective, SearchInputComponent],
+  exports: [
+    FontAwesomeModule,
+    ButtonDirective,
+    SearchInputComponent,
+    CardComponent,
+    CardSectionComponent,
+  ],
   providers: [TypeAheadService],
 })
 export class SharedModule {}

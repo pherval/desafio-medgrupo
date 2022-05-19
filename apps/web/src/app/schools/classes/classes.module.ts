a1f6467713731454c96256d3a2040808494aa91b
Peter <github@peter.dev.br>
2022-05-19 12:43:23 -0300

    feat(web): add component to create new classes to school

--- apps/web/src/app/schools/classes/classes.module.ts
+++ apps/web/src/app/schools/classes/classes.module.ts
@@ -4,9 +4,10 @@ import { ClassesComponent } from './classes.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SchoolsRoutingModule } from './classes-routing.module';
 import { ClassesService } from './classes.service';
+import { NewClassesComponent } from './new-classes/new-classes.component';
 
 @NgModule({
-  declarations: [ClassesComponent],
+  declarations: [ClassesComponent, NewClassesComponent],
   imports: [SharedModule, CommonModule, SchoolsRoutingModule],
   providers: [ClassesService],
 })
---
+++ apps/web/src/app/schools/classes/new-classes/new-classes.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-classes',
+  templateUrl: './new-classes.component.html',
+  styleUrls: ['./new-classes.component.scss'],
+})
+export class NewClassesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

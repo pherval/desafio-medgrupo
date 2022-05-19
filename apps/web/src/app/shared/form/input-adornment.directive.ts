3b529c2a6502fbe8e853ca02ca76b212f06c9fff
Peter <github@peter.dev.br>
2022-05-19 01:01:00 -0300

    fix(web): input adornment color

--- apps/web/src/app/shared/form/input-adornment.directive.ts
+++ apps/web/src/app/shared/form/input-adornment.directive.ts
@@ -1,8 +1,12 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputAdornment]',
 })
 export class InputAdornmentDirective {
   @Input('appInputAdornment') direction?: 'left' | 'right' = 'right';
+
+  @HostBinding('class') get classes() {
+    return 'text-slate-500';
+  }
 }

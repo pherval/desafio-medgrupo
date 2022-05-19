82feb24d2f5dc4db194ecc15b77b107240508ecd
Peter <github@peter.dev.br>
2022-05-19 00:40:48 -0300

    feat(web): add button themes

--- apps/web/src/app/shared/button.directive.ts
+++ apps/web/src/app/shared/button.directive.ts
@@ -11,16 +11,34 @@ import {
 })
 export class ButtonDirective {
   @Input() theme: keyof typeof this.themes = 'primary';
+  @Input() disabled: boolean = false;
 
   @HostBinding('class') get className() {
     return [
-      'py-2 px-4 flex flex-row gap-3 items-center cursor-pointer',
-      this.themes[this.theme ?? 'primary'],
+      'py-2 px-4 flex flex-row gap-3 items-center cursor-pointer rounded transition duration-100',
+      this.themes[this.theme ?? 'primary'][
+        this.disabled ? 'disabled' : 'default'
+      ],
     ].join(' ');
   }
 
   private themes = {
-    primary: 'bg-sky-600 text-gray-50',
-    outlined: 'text-slate-700 border border-slate-700',
+    primary: {
+      default: 'bg-sky-600 text-gray-50',
+      disabled: 'bg-gray-300 text-gray-500 cursor-not-allowed',
+    },
+    outlined: {
+      default: 'text-slate-700 border border-slate-700',
+      disabled: '',
+    },
+
+    secondary: {
+      default: 'bg-slate-300 rounded-lg text-slate-800',
+      disabled: '',
+    },
+    danger: {
+      default: 'bg-rose-700 text-slate-50',
+      disabled: '',
+    },
   };
 }
